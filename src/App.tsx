cd579fa29de0e15bb79076ab8cdd509096982a01
Marsikus93 <marusia234@gmail.com>
2022-12-02 20:27:23 +0200

    countertest

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Button1} from "./Button";
+
+// export type DataType = {
+//     count: 1|2|3|4|5
+// }
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let[count,setCount]=useState(0)
+    const onClickHandler = () => {
+        setCount(++count)
+    }
+    const resetHandler = () => {
+        setCount(0)
+    }
+    return (<div className="counter">
+            <div className="count">
+                <h1 className="title">{count}</h1>
+             <div className="buttons">   <Button1
+                    name={"inc"}
+                count={count}
+                    hide={true}
+                onClickHandler={onClickHandler}/>
+
+                <Button1
+                    name={"reset"}
+                    count={count}
+                    onClickHandler={resetHandler}
+                    hide={false}
+                /></div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export type ButtonType={
+    onClickHandler:()=>void
+    count:number
+    name:string
+    hide:boolean
+}
+
+export const Button1=(props:ButtonType)=>{
+    const maxcount=5
+    return(
+        <button className="b1" disabled={props.hide && props.count>=maxcount} onClick={props.onClickHandler}>{props.name}</button>
+    )
+}
